3dd5c0129ddf53f26e7251ce60b9b0f2f58b67a4
GrebVl <108603647+GrebVl@users.noreply.github.com>
2022-07-22 00:32:08 +0600

    Update Program.cs

--- taskDop/Program.cs
+++ taskDop/Program.cs
@@ -19,14 +19,14 @@ if (num == 1)
     double squareCir = 0;
     if (circle == 1)
     {
-        Console.WriteLine("ведите радиус");
+        Console.WriteLine("введите радиус");
         int radiusCircle = int.Parse(Console.ReadLine());
         squareCir = PI * Math.Pow(radiusCircle, 2);
         Console.WriteLine("площадь круга равна = " + squareCir);
     }
     else if (circle == 2)
     {
-        Console.WriteLine("ведите длину");
+        Console.WriteLine("введите длину");
         int distnsCircle = int.Parse(Console.ReadLine());
         squareCir = Math.Pow(distnsCircle, 2) / (4 * PI);
         Console.WriteLine("площадь круга равна = " + squareCir);
@@ -39,18 +39,18 @@ if (num == 1)
 }
 else if (num == 2)
 {
-    Console.WriteLine("ведите длину");
+    Console.WriteLine("введите длину");
     int lengthRec = int.Parse(Console.ReadLine());
-    Console.WriteLine("ведите ширину");
+    Console.WriteLine("введите ширину");
     int widthRec = int.Parse(Console.ReadLine());
     double squareRectangle = lengthRec * widthRec;
     Console.WriteLine("площадь прямоугольника равна = " + squareRectangle);
 }
 else if (num == 3)
 {
-    Console.WriteLine("ведите длину основания");
+    Console.WriteLine("введите длину основания");
     int lengthTri = int.Parse(Console.ReadLine());
-    Console.WriteLine("ведите высоту");
+    Console.WriteLine("введите высоту");
     int heightTri = int.Parse(Console.ReadLine());
     double squareTriangle = (lengthTri * heightTri) / 2;
     Console.WriteLine("площадь треугольника равна = " + squareTriangle);
